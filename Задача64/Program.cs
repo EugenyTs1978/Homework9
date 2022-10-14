3a5c06810e0e6666c7afd933a077cc9736e46b69
Eugeny1978 <eugenytsypyshev@rambler.ru>
2022-10-14 15:55:40 +0500

    Домашнее задание9

---
+++ Задача64/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1.
+ Выполнить с помощью рекурсии.
+*/
+void PrintArray(int Number)
+{
+    if (Number == 0)
+    {
+        return;
+    }
+    Console.Write(Number == 1? Number: Number + ",");
+    PrintArray(Number - 1);
+    
+}
+int userNumber = Convert.ToInt32(Console.ReadLine());
+PrintArray(userNumber);
