e4866ff29e346e0f8d9399321d2d939052a93879
Eugeny1978 <eugenytsypyshev@rambler.ru>
2022-10-14 16:51:26 +0500

    Домашнее задание9

---
+++ Задача66/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+*/
+int SumOfRange(int minNumber, int maxNumber)
+{
+    if (minNumber == maxNumber)
+    {
+        return minNumber;
+    }
+    return minNumber + SumOfRange(minNumber + 1, maxNumber);
+}
+
+int userMinNum = Convert.ToInt32(Console.ReadLine());
+int userMaxNum = Convert.ToInt32(Console.ReadLine());
+int userSum = userMaxNum>userMinNum?SumOfRange(userMinNum, userMaxNum):SumOfRange(userMaxNum, userMinNum);
+Console.WriteLine(userSum);
+
